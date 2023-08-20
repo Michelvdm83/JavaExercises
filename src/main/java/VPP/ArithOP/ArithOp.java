be2778ee5b59b94f1957934ff2484ddcaa3f643d
Michelvdm83 <michelvdm83@gmail.com>
2023-08-20 11:56:14 +0200

    test2

--- src/main/java/VPP/ArithOP/ArithOp.java
+++ src/main/java/VPP/ArithOP/ArithOp.java
@@ -1,2 +1,14 @@
-package VPP.ArithOP;public class ArithOp {
+package VPP.ArithOP;
+
+public class ArithOp {
+    public static void main(String[] args){
+        System.out.println(-5+8*6);
+        System.out.println((55+9)%9);
+        System.out.println(20+-3*5/8);
+        System.out.println(5+15/3*2-8%3);
+/*a. -5 + 8 * 6
+b. (55+9) % 9
+c. 20 + -3*5 / 8
+d. 5 + 15 / 3 * 2 - 8 % 3*/
+    }
 }
