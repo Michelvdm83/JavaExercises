cce272d92937da0a7fa47ac4d63b070126828353
Michelvdm83 <michelvdm@gmail.com>
2023-11-24 16:32:42 +0100

    onwards

---
+++ src/main/java/VPP/TheVariableShop/TheVariableShop.java
@@ -0,0 +1,42 @@
+package VPP.TheVariableShop;
+
+public class TheVariableShop {
+    public static void main(String[] args){
+        double dVar = 0.0;
+        int iVar = 1;
+        boolean boVar = true;
+        byte byVar = 127;
+        short shVar = 2;
+        long lVar = 1L;
+        char cVar = 'b';
+        float fVar = 12F;
+        String sVar = "bey";
+        System.out.println(dVar);
+        System.out.println(iVar);
+        System.out.println(boVar);
+        System.out.println(byVar);
+        System.out.println(shVar);
+        System.out.println(lVar);
+        System.out.println(cVar);
+        System.out.println(fVar);
+        System.out.println(sVar);
+
+    }
+}
+/*
+The Variable Shop
+You see an old shopkeeper struggling to stack up variables in a window display.
+“Hoowee! All these variable types sure are exciting but setting them all up to
+show them
+off to excited new programmers like yourself is a lot of work for these aching
+bones,”
+she says. “You wouldn’t mind helping me set up this program with one variable
+of every
+type, would you?”
+
+Objectives:
+- Build a program with a variable of all eight primitive Java types (as well as
+the 'text' type).
+- Assign each of them a value using a literal of the correct type.
+- Use System.out.println to display the contents of each variable.
+ */
